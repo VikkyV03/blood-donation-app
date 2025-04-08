6334555ed8de10a3a6ee05eefd285361e005ae8a
Gouthamv99 <gauthamkiranv1998@gmail.com>
2025-04-07 21:36:45 -0500

    Fix: Load Firebase credentials from Render secret file again

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,9 +1,7 @@
 // backend/firebase.js
-
 const admin = require('firebase-admin');
 const fs = require('fs');
 
-// Read and parse the Firebase service account secret
 const serviceAccount = JSON.parse(
   fs.readFileSync('/etc/secrets/FIREBASE_SERVICE_ACCOUNT_KEY', 'utf8')
 );
